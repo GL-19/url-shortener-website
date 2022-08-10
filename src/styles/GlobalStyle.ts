aa5a180aef6b207aecf647a066d1666e4d7bbf5f
GL-19 <loiolagl19@gmail.com>
2022-08-10 18:41:00 -0300

    feat: add GlobalStyle

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,5 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --cyan: hsl(180, 66%, 49%);
+    --dark-violet: hsl(257, 27%, 26%);
+    --red: hsl(0, 87%, 67%);
+    
+    --gray: hsl(0, 0%, 75%);
+    --grayish-violet: hsl(257, 7%, 63%);
+    --very-dark-blue: hsl(255, 11%, 22%);
+    --very-dark-violet: hsl(260, 8%, 14%);
+  }
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 16px;
+    font-family: 'Poppins', sans-serif;
+  }
 `;
